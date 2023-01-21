a38e2cd145eb7fc2c81a39e7ca8ec4c00690993a
Sergey <Seregsa2014@gmail.com>
2023-01-21 19:47:37 +0300

    Update
    animation with black screen added

---
+++ Assets/Scripts/BlackScreenAnim.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlackScreenAnim : MonoBehaviour
+{
+    [SerializeField] private float animationTime = 2f;
+    private bool inCutscene = false;
+    private Image ObjectImage;
+    private Color fullColor;
+    private Color transparentColor;
+
+    private void Awake()
+    {
+        ObjectImage = GetComponent<Image>();
+        fullColor = ObjectImage.color;
+        fullColor.a = 1f;
+        transparentColor = ObjectImage.color;
+        transparentColor.a = 0f;
+    }
+
+    public void PlayAnimation(float animationPauseTime)
+    {
+        StartCoroutine(anim(animationPauseTime));
+        Debug.Log("Black screen");
+    }
+
+    public float giveAnimationTime()
+    {
+        return animationTime;
+    }
+
+    public bool giveInCutsceneState()
+    {
+        return inCutscene;
+    }
+
+    private IEnumerator anim(float animationPauseTime)
+    {
+        float time = 0;
+        float step = 1f / animationTime;
+
+        while (time < animationTime)
+        {
+            time += Time.deltaTime;
+            ObjectImage.color = Color.Lerp(transparentColor, fullColor, step * time);
+            yield return null;
+        }
+        inCutscene = true;
+        yield return new WaitForSeconds(animationPauseTime);
+        inCutscene = false;
+        time = 0;
+        while (time < animationTime)
+        {
+            time += Time.deltaTime;
+            ObjectImage.color = Color.Lerp(fullColor, transparentColor, step * time);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+
 public class CameraController : MonoBehaviour
 {
     public static CameraController Instance { get; private set; }
