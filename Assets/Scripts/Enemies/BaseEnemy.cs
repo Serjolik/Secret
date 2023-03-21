7db13ac3dc517d639d86e851e944a78f8ef119ff
Sergey <Seregsa2014@gmail.com>
2023-03-21 14:22:45 +0300

    Update BaseEnemy.cs
    Check nearest position added, and some params and headers

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour
 {
+    [Header("Enemy params:")]
     [SerializeField] private float hp = 100; // procent
+    [SerializeField] private float damage = 3;
     [SerializeField] private float speed = 2f;
 
+    [Header("Points positions")]
     [SerializeField] private List<Vector3> pointsPositions;
 
     enum State
@@ -49,6 +52,31 @@ public class BaseEnemy : MonoBehaviour
     private void SearchState()
     {
 
+    }
+
+    private void CheckCurrentPosition()
+    {
+        int nearestPointId;
+        float minDistance = 10000;
+        int index = 0;
+        foreach(Vector3 position in pointsPositions)
+        {
+            float localDistance = Vector3.Distance(transform.position, position);
+
+            if (localDistance < minDistance)
+            {
+                minDistance = localDistance;
+                nearestPointId = index;
+            }
+
+            index++;
+        }
+    }
+
+    private void ChangeState(State newState)
+    {
+        state = newState;
+        CheckCurrentPosition();
     }
 
 }
